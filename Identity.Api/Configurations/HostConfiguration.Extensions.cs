8bcf284db8697eca8cee42b264aefead653078cd
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-05-18 15:31:44 +0500

    Added host configuration and xml fils

---
+++ Identity.Api/Configurations/HostConfiguration.Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Identity.Domain.Constants;
+using Identity.Persistence.DataContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static readonly ICollection<Assembly> Assemblies = Assembly
+        .GetExecutingAssembly()
+        .GetReferencedAssemblies()
+        .Select(Assembly.Load)
+        .Append(Assembly.GetExecutingAssembly())
+        .ToList();
+
+    
+    /// <summary>
+    /// Registers persistence infrastructure
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+    {
+        // Determine connection string
+        var dbConnectionString = builder.Environment.IsProduction()
+            ? Environment.GetEnvironmentVariable(DataAccessConstant.DbConnectionString)
+            : builder.Configuration.GetConnectionString(DataAccessConstant.DbConnectionString);
+        
+        // Register data context
+        builder.Services.AddDbContext<AppDbContext>(options => { options.UseNpgsql(dbConnectionString); });
+        
+        return builder;
+    }
+    
+}
---
+++ Identity.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Identity.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddPersistence();
+
+        return new(builder);
+    }
+
+    public static async ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        return app;
+    }
+}
